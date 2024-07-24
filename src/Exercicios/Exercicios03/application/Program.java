57457c6b9a902f0f4142b9262e34daa51e8547f4
lucas <lucasgarciaamorim@gmail.com>
2024-07-23 23:43:34 -0300

    Aula 70 - Exercício 3

---
+++ src/Exercicios/Exercicios03/application/Program.java
@@ -0,0 +1,36 @@
+package Exercicios.Exercicios03.application;
+
+import Exercicios.Exercicios03.entities.Estudent;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Estudent estudent = new Estudent();
+
+        System.out.println("Name: ");
+        estudent.name = sc.nextLine();
+
+        System.out.println("Grade: ");
+        estudent.grade1 = sc.nextDouble();
+        estudent.grade2 = sc.nextDouble();
+        estudent.grade3 = sc.nextDouble();
+
+        System.out.printf("FINAL GRADE %.2f%n", estudent.finalGrade());
+
+        if (estudent.finalGrade() < 60) {
+            System.out.println("FAILED");
+            System.out.printf("MISSING %.2f POINTS%n", estudent.missingPoint());
+        } else {
+            System.out.println("PASS");
+
+        }
+        sc.close();
+    }
+
+}
