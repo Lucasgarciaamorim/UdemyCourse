5c5f1f0c15bcdcee18a6329de86491243fd75ebd
lucas <lucasgarciaamorim@gmail.com>
2024-07-29 21:29:09 -0300

    Aula 90 - Seção 10 - Exemplo Vetores ( ajuste para lenght)

--- src/Seção10/Vetores/application/Program.java
+++ src/Seção10/Vetores/application/Program.java
@@ -15,14 +15,14 @@ public class Program {
         Product[] vect = new Product[n];
 
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < vect.length; i++) {
             sc.nextLine();
             String name = sc.nextLine();
             double price = sc.nextDouble();
             vect[i] = new Product(name, price);
         }
         double sum = 0.0;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < vect.length; i++) {
             sum += vect[i].getPrice();
         }
         double avg = sum / vect.length;
