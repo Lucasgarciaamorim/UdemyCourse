7b2c5f3033f068681c3a0fdc78ef6b65426bfeb6
lucas <lucasgarciaamorim@gmail.com>
2024-08-05 22:34:12 -0300

    Seção 10 - List

---
+++ src/Seção10/List/program.java
@@ -0,0 +1,17 @@
+package Seção10.List;
+
+//Tamanho da lista: size()
+//Inserir elemento na lista: add(obj), add(int, obj)
+//Remover elementos da lista: remove(obj), remove(int), removeIf(Predicate)
+//Encontrar posição de elemento: indexOf(obj), lastIndexOf(obj)
+//Filtrar lista com base em predicado:
+// List<Integer> result = list.stream().filter(x -> x > 4).collect(Collectors.toList())
+//Encontrar primeira ocorrência com base em predicado:
+//Integer result = list.stream().filter(x -> x > 4).findFirst().orElse(null)
+
+
+public class program {
+    public static void main(String[] args) {
+
+    }
+}
